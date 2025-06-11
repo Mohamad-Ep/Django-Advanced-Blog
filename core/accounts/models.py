449d89955d479b3c1a3540d95fc534f4457f8afb
Mohamad Ep <mamad_ep@yahoo.com>
2025-06-11 18:31:23 +0330

    Feat: Create accounts & blog app

---
+++ core/accounts/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+# __________________________________________________________
