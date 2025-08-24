cda5858e143b340249e7941c050c822275efcd97
Mohamad Ep <mamad_ep@yahoo.com>
2025-08-24 18:38:53 +0330

    Feat: Preparation for deploy project

--- core/core/settings.py
+++ core/core/settings.py
@@ -141,9 +141,9 @@ USE_TZ = False
 # media & static config
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIR = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -203,7 +203,7 @@ SIMPLE_JWT = {
 
 # __________________________________________________________
 
-# Email Settings
+# Develop Email Settings
 
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"        # برای تست از طریق کنسول
 
---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
